6ae47b40cb748a5c2bc5c97bc6c802d64aef830d
Lucas Barra <lucastbarra@hotmail.com>
2021-02-23 12:00:21 -0300

    NLW#4 day 2 pt.1

---
+++ src/components/profile.tsx
@@ -0,0 +1,12 @@
+export function Profile(){
+    return (
+        <div>
+            <img src="https://github.com/lucastrindadebarra.png" alt="Lucas Barra"/>
+            <div>
+                <strong>Lucas Barra</strong>
+
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
